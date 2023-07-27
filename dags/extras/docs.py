cc478b2878df67fb1998458d67f6a05ad16f6ee9
fritz-astronomer <fritz@astronomer.io>
2023-07-27 17:38:44 -0400

    update fitness test doc

--- dags/extras/docs.py
+++ dags/extras/docs.py
@@ -1,11 +1,10 @@
 doc = """
-# Fitness Test
-1) Run a sql operator with a connection w/ a xcom + template
-    - SQLExecuteQueryOperator(sql="SELECT 1;", conn_id="...")
-2) do it with an older version of the `PostgresOperator` 
-3) Run a bash operator with ruby, w/ a xcom + template
-    - BashOperator(bash_command=\"\"\"ruby -e 'puts "Hello world \{\{\}\}"'\"\"\")
-4) Run a python operator, invoking a fn in the dag that imports a fn in the include folder, w/ a xcom + template
-5) Do it #4 in python2.7
-6) Combine the #4 + #5 via xcom and print if they match
+## Fitness Test
+1) Run a SQL Operator with an Airflow Connection, return results as an XCOM, and use an Airflow Template in the SQL
+2) Do #1 with an older version of the `PostgresOperator` than the one installed in the parent `requirements.txt`
+3) Run `ruby` with an and Airflow Template and return results as an XCOM
+4) Run a Python function that consumes an Airflow Template and is written alongside the DAG, importing a function in the `/include` folder. Return the results as an XCOM
+5) Do #4 in python2.7
+6) Combine the #4 + #5 via consuming an XCOM and print if they match
+
 """
